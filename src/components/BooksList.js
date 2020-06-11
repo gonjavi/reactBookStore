6d90b336f32048c0acb389e5318310807012aef3
gonjavi <javiercaliescali@gmail.com>
2020-06-11 18:00:53 -0500

    booklist.js

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -2,44 +2,37 @@ import React from 'react';
 import Table from 'react-bootstrap/Table';
 import { connect } from 'react-redux';
 
-class BooksList extends React.Component {
-  render() {
-    const { books } = this.props;
-    const bookList = books ? (
-      books.map(book => {
-        return (
-          <tbody>
-            <tr>
-              <td>{book.id}</td>
-              <td>{book.title}</td>
-              <td>{book.category}</td>
-            </tr>
-          </tbody>
-        );
-      })
-    ) : (
-      <div>No Books available</div>
-    );
-    return (
-      <Table striped bordered hover>
-        <thead>
+const BooksList = books => { 
+  let bookList;
+  if (books) {
+    books.map(book => {
+      bookList =(
+        <tbody key={book.id}>
           <tr>
-            <th>BookID</th>
-            <th>Title</th>
-            <th>Category</th>
+            <td>{book.id}</td>
+            <td>{book.title}</td>
+            <td>{book.category}</td>
           </tr>
-        </thead>
-        {bookList}
-      </Table>
-    );
-  }
+        </tbody>      
+      );
+    }
+  },
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>BookID</th>
+          <th>Title</th>
+          <th>Category</th>
+        </tr>
+      </thead>
+      {bookList}
+    </Table>
+  );
 }
 
-const mapStateToProps = (state, ownProps) => {
-  const id = ownProps.match.params.book_id;
-  return {
-    books: state.books.find(book => book.id === id),
-  };
+const mapStateToProps = state => {  
+  return { books: state.books };
 };
 
 export default connect(mapStateToProps)(BooksList);
