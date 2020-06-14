4093e0df7ea0b4b540bdca658ee4de8bb7c0be93
gonjavi <javiercaliescali@gmail.com>
2020-06-14 16:18:43 -0500

    bootstrap

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import BooksForm from '../containers/BookForm';
