688e2ee134033abc72a1a08cb744bd1c6118f012
gonjavi <javiercaliescali@gmail.com>
2020-06-26 15:21:35 -0500

    add icon to navbar

--- src/containers/NavBar.js
+++ src/containers/NavBar.js
@@ -7,6 +7,7 @@ const NavBar = () => (
       <div className="logo">Bookstore CMS</div>
       <div className="titles">Books</div>
       <div className="titles">Categories</div>
+      <div className="icon"><i className="fa fa-user fa-2x icon-in" aria-hidden="true" /></div>
     </Navbar>
   </div>
 );
